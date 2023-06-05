f314d88c1865e9f87f09cf831b947c2ed14dae3f
official-abhishek <135330349+official-abhishek@users.noreply.github.com>
2023-06-05 12:40:26 +0530

    Update ServiceImpl.java

--- src/main/java/com/innostax/CRUD/service/ServiceImpl.java
+++ src/main/java/com/innostax/CRUD/service/ServiceImpl.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,10 +27,8 @@ public class ServiceImpl implements ServiceInter {
 		for (User user : list) {
 			Slist.add(user.getName());
 		}
-		
 
 		Collections.sort(Slist);
-		
 
 		return resultGen(Slist, list);
 	}
@@ -40,19 +36,17 @@ public class ServiceImpl implements ServiceInter {
 	private static List<User> resultGen(ArrayList<String> Slist, List<User> list) {
 		int n = Slist.size();
 		List<User> result = new ArrayList<>();
-		System.out.println(Slist) ;
-		System.out.println(list) ;
+		System.out.println(Slist);
+		System.out.println(list);
 		for (int i = 0; i < n; i++) {
-			String check = Slist.get(i) ; 
-			for(int j = 0 ; j < list.size() ; j ++) {
-				if(list.get(j).getName().toString().equals(check)) {
-					result.add(list.get(j)) ;
-					list.remove(j) ;
+			String check = Slist.get(i);
+			for (int j = 0; j < list.size(); j++) {
+				if (list.get(j).getName().toString().equals(check)) {
+					result.add(list.get(j));
+					list.remove(j);
 				}
 			}
 		}
-		
-		
 
 		return result;
 	}
